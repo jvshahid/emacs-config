c8aafa16b35349ecb6757920ee9a8c450370ca37
John Shahid <jvshahid@gmail.com>
2013-01-19 15:31:37 -0500

    make the generate tags script smarter.

--- generate_tags.sh
+++ generate_tags.sh
@@ -1,2 +1,26 @@
 #! /bin/bash
-ctags-exuberant -R --extra=+fq -e --langdef=scala --langmap=scala:.scala --langdef=otq --langmap=otq:.otq --langdef=xml --langmap=xml:.xml --langdef=properties --langmap=properties:.properties "$@"
+
+function show_usage () {
+    echo "Usage: $0 <type>"
+
+    echo "Where type is the type of the project and can be one of:"
+    echo "  1. Java"
+    echo "  2. Ruby"
+    echo "  2. C++ (imply C and C++)"
+    exit 1
+}
+
+if [ $# -ne 1 ]; then
+    show_usage
+fi
+
+case $1 in
+    Java|java) languages="Java,Ant";;
+    Ruby|ruby) languages="Ruby";;
+    C++|c++)   languages="C,C++";;
+    *)         show_usage;;
+esac
+
+language_definitions="--langdef=scala --langmap=scala:.scala --langdef=otq --langmap=otq:.otq --langdef=xml --langmap=xml:.xml --langdef=properties --langmap=properties:.properties"
+
+ctags-exuberant -R --extra=+fq -e --languages="$languages" "$language_definitions"
