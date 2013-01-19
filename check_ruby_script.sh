deef10168f601c19dde4e0f8c047aa158c07211f
John Shahid <jvshahid@gmail.com>
2013-01-19 15:00:18 -0500

    quote the output of the `ruby -c' script. add some documentation.

--- check_ruby_script.sh
+++ check_ruby_script.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+# This script will remove the shebang line and pass the
+# rest of the file to `ruby -c' to check for parse errors
 output=$(cat $1 | sed '1s/#!.*//' | ruby -c 2>&1)
 exit_status=$?
 file_name=$(echo $1 | sed 's/\//\\\//g')
-echo $output | sed "s/^-:/$file_name:/"
+echo "$output" | sed "s/^-:/$file_name:/"
 exit $exit_status
