bb1c93a8f82629c8f427a5835dbc5be74c31a108
John Shahid <jvshahid@gmail.com>
2013-01-27 14:47:36 -0500

    add support for GO.

--- generate_tags.sh
+++ generate_tags.sh
@@ -6,7 +6,8 @@ function show_usage () {
     echo "Where type is the type of the project and can be one of:"
     echo "  1. Java"
     echo "  2. Ruby"
-    echo "  2. C++ (imply C and C++)"
+    echo "  3. C++ (imply C and C++)"
+    echo "  4. GO"
     exit 1
 }
 
@@ -18,9 +19,10 @@ case $1 in
     Java|java) languages="Java,Ant";;
     Ruby|ruby) languages="Ruby";;
     C++|c++)   languages="C,C++";;
+    GO)        languages="GO,C,C++";;
     *)         show_usage;;
 esac
 
 language_definitions="--langdef=scala --langmap=scala:.scala --langdef=otq --langmap=otq:.otq --langdef=xml --langmap=xml:.xml --langdef=properties --langmap=properties:.properties"
 
-ctags-exuberant -R --extra=+fq -e --languages="$languages" "$language_definitions"
+~/bin/ctags/bin/ctags -R --extra=+fq -e --languages="$languages" "$language_definitions"
