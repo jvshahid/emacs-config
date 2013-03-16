37824edcff515d05bfb221af97f5e7fdd0d6a6f1
John Shahid <jvshahid@gmail.com>
2013-03-16 13:08:13 -0400

    distributed shell alias.

--- alias.sh
+++ alias.sh
@@ -12,3 +12,5 @@ alias gen_tags="$HOME/Documents/generate_tags.sh"
 alias mosh="$MOSH_INSTALLATION/bin/mosh --client=$MOSH_INSTALLATION/bin/mosh-client --server='$MOSH_INSTALLATION/bin/mosh-server'"
 alias wdiff="wdiff -n -w $'\033[1;31m' -x $'\033[0m' -y $'\033[1;34m' -z $'\033[0m'"
 alias ll='ls -l -a --color'
+# dsh interactive, wait for new commands and execute them
+alias dshi="dsh -i -c -M"
