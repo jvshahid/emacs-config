361c483f610901a969143aa4ae587d11c7980d9e
John Shahid <jvshahid@gmail.com>
2013-08-30 12:28:00 -0400

    make the bin files executable. change the owner on the files.

--- cookbooks/development/recipes/basic.rb
+++ cookbooks/development/recipes/basic.rb
@@ -9,5 +9,8 @@ directory "#{ENV['HOME']}/bin"
 files = ['alias.sh', 'functions.sh', 'exports.sh', 'prompt.sh', '.ackrc', '.gdbinit', '.zshrc', 'bin/generate_tags.sh',
          'bin/cleanup_shit.sh', '.emacs', '.gitconfig']
 files.each do |file|
-  cookbook_file "#{ENV['HOME']}/#{file}"
+  cookbook_file "#{ENV['HOME']}/#{file}" do
+    mode 0755 if file =~ /bin/
+    owner "jvshahid"
+  end
 end
