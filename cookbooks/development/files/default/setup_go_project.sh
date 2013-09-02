045471f586468ace8fbe17b6f0fa6d7ae315ed3f
John Shahid <jvshahid@gmail.com>
2013-09-02 17:24:15 -0400

    start emacs as well.

--- cookbooks/development/files/default/setup_go_project.sh
+++ cookbooks/development/files/default/setup_go_project.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-cd `pwd $0`
 [ ! -f ./exports.sh ] && echo "Please create a exports.sh file" && exit 1
 
 . ./exports.sh
@@ -9,3 +8,4 @@ go get -u code.google.com/p/rog-go/exp/cmd/godef
 go get -u github.com/nsf/gocode
 cp bin/godef ~/bin/
 cp bin/gocode ~/bin/
+emacs-snapshot --no-desktop &
