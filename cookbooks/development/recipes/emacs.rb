9206e8e3f7b6470970722e6063b942f189a3971e
John Shahid <jvshahid@gmail.com>
2015-04-05 11:33:02 -0400

    Get rid of commented code

--- cookbooks/development/recipes/emacs.rb
+++ cookbooks/development/recipes/emacs.rb
@@ -1,15 +1,3 @@
 link "#{ENV['HOME']}/.emacs.d" do
   to File.expand_path("../../../emacs.d", File.dirname(__FILE__))
 end
-
-bash "build ensime" do
-  # TODO: port this to ruby
-
-  # [ -f ./sbt ] || curl 'https://raw.github.com/paulp/sbt-extras/master/sbt' -o ./sbt
-  # chmod a+x ./sbt
-  # [ -d target/dist ] || ./sbt stage
-  # dist_dir=$(readlink -f $(ls -d dist*))
-  # ln -s $dist_dir $HOME/.emacs.d/libs/ensime_head
-  # popd
-
-end
