624e376ec5f729ae1235994f32449f30dad5f0e9
John Shahid <jvshahid@gmail.com>
2016-02-18 17:15:46 -0500

    adds .gitignore_global

--- cookbooks/development/recipes/basic.rb
+++ cookbooks/development/recipes/basic.rb
@@ -3,7 +3,7 @@ directory "#{ENV['HOME']}/bin"
 files = ['alias.sh', 'functions.sh', 'exports.sh', 'prompt.sh',
          '.ackrc', '.gdbinit', '.zshrc', 'bin/generate_tags.sh',
          'bin/cleanup_shit.sh', 'bin/setup_go_project.sh', '.emacs',
-         '.gitconfig', '.clang-format']
+         '.gitconfig', '.gitignore_global', '.clang-format']
 
 files.each do |file|
   cookbook_file "#{ENV['HOME']}/#{file}" do
