1ad71847e25139e1e8360fe79ae0967e744566d0
John Shahid <jshahid@pivotal.io>
2016-07-05 12:56:47 -0400

    force push

--- scripts/lw-setup.sh
+++ scripts/lw-setup.sh
@@ -3,7 +3,7 @@ scripts_dir=`dirname $0`
 cd $scripts_dir/..
 repo_dir=$PWD
 rm -rf ~/.emacs.d
-rm ~/.emacs ~/.magit.emacs
+rm -f ~/.emacs ~/.magit.emacs
 ln -s $repo_dir/emacs.d ~/.emacs.d
 ln -s $repo_dir/cookbooks/development/files/default/.emacs ~/
 ln -s $repo_dir/cookbooks/development/files/default/.magit.emacs ~/
