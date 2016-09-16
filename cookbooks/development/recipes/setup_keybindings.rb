1b200602568f08116264c22ee93f4b0b3cf283c8
John Shahid <jvshahid@gmail.com>
2016-09-16 13:37:24 -0400

    setup microphone muting key properly
    this is a bit hacky, sicne KYE_MICMUTE isn't a supported event by the
    keyboard, i ended up mapping the mute key to KEY_RECORD and adding a
    custom keybinding to use pulse audio cli to mute the microphone

--- cookbooks/development/recipes/setup_keybindings.rb
+++ cookbooks/development/recipes/setup_keybindings.rb
@@ -11,9 +11,13 @@ dconf write /org/cinnamon/muffin/keybindings/move-to-workspace-left "['<Super>l'
 dconf write /org/cinnamon/muffin/keybindings/switch-to-workspace-left "['<Super>j']"
 dconf write /org/cinnamon/muffin/keybindings/switch-to-workspace-right "['<Super>k']"
 
-dconf write /org/cinnamon/keybindings/custom-list "['custom0']"
+dconf write /org/cinnamon/keybindings/custom-list "['custom0','custom1']"
 dconf write /org/cinnamon/keybindings/custom-keybindings/custom0/name "'diodon'"
 dconf write /org/cinnamon/keybindings/custom-keybindings/custom0/binding "'<Control><Shift>V'"
 dconf write /org/cinnamon/keybindings/custom-keybindings/custom0/command "'/usr/bin/diodon'"
+
+dconf write /org/cinnamon/keybindings/custom-keybindings/custom1/name "'mute microphone'"
+dconf write /org/cinnamon/keybindings/custom-keybindings/custom1/binding "'XF86AudioMicMute'"
+dconf write /org/cinnamon/keybindings/custom-keybindings/custom1/command "'amixer set Capture toggle'"
 EOF
 end
