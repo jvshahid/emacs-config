3938457c4eb0cf17f2b5239cd67bcf077d4123c3
John Shahid <jvshahid@gmail.com>
2017-03-19 11:32:23 -0400

    fix things for linux mint 18.1

--- cookbooks/development/recipes/setup_keybindings.rb
+++ cookbooks/development/recipes/setup_keybindings.rb
@@ -6,18 +6,26 @@ gconftool --type string --set /apps/gnome-terminal/keybindings/move_tab_right '<
 gconftool --type string --set /apps/gnome-terminal/keybindings/next_tab '<Primary>bracketright'
 gconftool --type string --set /apps/gnome-terminal/keybindings/prev_tab '<Primary>bracketleft'
 
-dconf write /org/cinnamon/muffin/keybindings/move-to-workspace-right "['<Super>semicolon']"
-dconf write /org/cinnamon/muffin/keybindings/move-to-workspace-left "['<Super>l']"
-dconf write /org/cinnamon/muffin/keybindings/switch-to-workspace-left "['<Super>j']"
-dconf write /org/cinnamon/muffin/keybindings/switch-to-workspace-right "['<Super>k']"
+# setup custom keybindings to paste using diodon
+dconf write /org/cinnamon/desktop/keybindings/custom-list "['custom0']"
+dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom0/name "'diodon'"
+dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom0/command "'/usr/bin/diodon'"
+dconf write /org/cinnamon/desktop/keybindings/custom-keybindings/custom0/binding "['<Primary><Shift>V']"
 
-dconf write /org/cinnamon/keybindings/custom-list "['custom0','custom1']"
-dconf write /org/cinnamon/keybindings/custom-keybindings/custom0/name "'diodon'"
-dconf write /org/cinnamon/keybindings/custom-keybindings/custom0/binding "'<Control><Shift>V'"
-dconf write /org/cinnamon/keybindings/custom-keybindings/custom0/command "'/usr/bin/diodon'"
+# setup keybindings
+dconf write /org/cinnamon/desktop/keybindings/wm/move-to-workspace-right "['<Super>semicolon']"
+dconf write /org/cinnamon/desktop/keybindings/wm/move-to-workspace-left "['<Super>l']"
+dconf write /org/cinnamon/desktop/keybindings/wm/switch-to-workspace-left "['<Super>j']"
+dconf write /org/cinnamon/desktop/keybindings/wm/switch-to-workspace-right "['<Super>k']"
+dconf write /org/cinnamon/desktop/keybindings/looking-glass-keybinding '[""]'
 
-dconf write /org/cinnamon/keybindings/custom-keybindings/custom1/name "'mute microphone'"
-dconf write /org/cinnamon/keybindings/custom-keybindings/custom1/binding "'XF86AudioMicMute'"
-dconf write /org/cinnamon/keybindings/custom-keybindings/custom1/command "'amixer set Capture toggle'"
+# disable sound effects
+dconf write /org/cinnamon/sounds/login-enabled false
+dconf write /org/cinnamon/sounds/logout-enabled false
+dconf write /org/cinnamon/sounds/unplug-enabled false
+dconf write /org/cinnamon/sounds/tile-enabled false
+dconf write /org/cinnamon/sounds/plug-enabled false
+dconf write /org/cinnamon/sounds/switch-enabled false
+
 EOF
 end
