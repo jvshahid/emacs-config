02a740e9d736aad708602984dc7da5affd541751
John Shahid <jvshahid@gmail.com>
2017-05-16 06:31:50 -0400

    use the same source file that chrome configures

--- cookbooks/development/recipes/chrome.rb
+++ cookbooks/development/recipes/chrome.rb
@@ -1,8 +1,10 @@
+path = "/etc/apt/sources.list.d/google-chrome.list"
+
 bash "add chrome ppa" do
   code <<-EOF
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-    sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+    sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> #{path}'
     sudo apt-get update
   EOF
-  not_if "grep -R chrome /etc/apt/sources.list.d/ > /dev/null 2>&1"
+  not_if { ::File.exist?(path) }
 end
