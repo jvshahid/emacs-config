215640aab5f34212248223bee336435cbbaee903
John Shahid <jvshahid@gmail.com>
2017-07-10 09:50:04 -0400

    only link .emacs.d

--- scripts/lw-setup.sh
+++ scripts/lw-setup.sh
@@ -5,5 +5,3 @@ repo_dir=$PWD
 rm -rf ~/.emacs.d
 rm -f ~/.emacs ~/.magit.emacs
 ln -s $repo_dir/emacs.d ~/.emacs.d
-ln -s $repo_dir/cookbooks/development/files/default/.emacs ~/
-ln -s $repo_dir/cookbooks/development/files/default/.magit.emacs ~/
