68a3f11acf4dc8ede79dffe618e63b7a6279d505
John Shahid <jvshahid@gmail.com>
2018-01-08 23:04:34 -0500

    use emacs to enter the email password

--- cookbooks/development/recipes/basic.rb
+++ cookbooks/development/recipes/basic.rb
@@ -4,7 +4,7 @@ files = ['alias.sh', 'functions.sh', 'exports.sh', 'prompt.sh',
   '.ackrc', '.gdbinit', '.zshrc', '.gemrc', 'bin/generate_tags.sh',
   'bin/flavor', 'bin/cleanup_shit.sh', 'bin/setup_go_project.sh',
   '.gitconfig', '.gitignore_global', '.clang-format', 'bin/diff-highlight',
-  '.tmux.conf', '.mbsyncrc']
+  '.tmux.conf', '.mbsyncrc', 'bin/read-password-emacs']
 
 files.each do |file|
   cookbook_file "#{ENV['HOME']}/#{file}" do
