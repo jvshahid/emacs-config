e009692d86b09e44ebae9c9535d190c93de4e644
John Shahid <jvshahid@gmail.com>
2018-01-19 11:17:38 -0500

    fix the init script

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -9,13 +9,14 @@
 
 (cask-initialize)
 
-(yas-reload-all)
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 ;; (package-initialize)
+
+(with-eval-after-load 'yasnippet-snippets
+  (yas-reload-all))
 
 (winner-mode)
 (global-set-key (kbd "C-c \\") 'split-window-horizontally)
