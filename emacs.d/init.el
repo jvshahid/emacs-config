88d052fd50fa6f642e6a702c009e2cce8019a370
John Shahid <jvshahid@gmail.com>
2018-01-19 10:52:34 -0500

    reload all snippets on startup

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -9,6 +9,8 @@
 
 (cask-initialize)
 
+(yas-reload-all)
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
