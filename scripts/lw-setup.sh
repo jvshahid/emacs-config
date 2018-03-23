1c81e233db2c6160b2dc48a453e9fb32c279f745
John Shahid <jvshahid@gmail.com>
2018-03-23 17:02:36 -0400

    autosetup docker to the required version

--- scripts/lw-setup.sh
+++ scripts/lw-setup.sh
@@ -4,4 +4,14 @@ cd $scripts_dir/..
 repo_dir=$PWD
 rm -rf ~/.emacs.d
 rm -f ~/.emacs ~/.magit.emacs
-ln -s $repo_dir/emacs.d ~/.emacs.d
+ln -sf $repo_dir/emacs.d ~/.emacs.d
+ln -sf $repo_dir/docker/ec ~/bin
+ln -sf $repo_dir/docker/ed ~/bin
+docker_version=$(docker version | sed -n '/Server:/,$P' | grep Version: | cut -d: -f2 | tr -d ' \t\n')
+minimum_docker_version=18.0.0
+higher_version=$(echo -e "$minimum_docker_version\n$docker_version" | sort -V | tail -n+2)
+if [ "x$higher_version" != "x$docker_version" ]; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get -y install docker-ce
+fi
