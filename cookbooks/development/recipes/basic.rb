576e5d5ba4883614781888b56aa82cfa4ad82cf1
John Shahid <jvshahid@gmail.com>
2018-03-24 10:23:46 -0400

    add bin/ed and bin/ec

--- cookbooks/development/recipes/basic.rb
+++ cookbooks/development/recipes/basic.rb
@@ -1,10 +1,11 @@
 directory "#{ENV['HOME']}/bin"
 
 files = ['alias.sh', 'functions.sh', 'exports.sh', 'prompt.sh',
-  '.ackrc', '.gdbinit', '.zshrc', '.gemrc', 'bin/generate_tags.sh',
-  'bin/flavor', 'bin/cleanup_shit.sh', 'bin/setup_go_project.sh',
-  '.gitconfig', '.gitignore_global', '.clang-format', 'bin/diff-highlight',
-  '.tmux.conf', '.mbsyncrc', 'bin/read-password-emacs', '.gnupg/gpg-agent.conf']
+         '.ackrc', '.gdbinit', '.zshrc', '.gemrc', 'bin/generate_tags.sh',
+         'bin/flavor', 'bin/cleanup_shit.sh', 'bin/setup_go_project.sh',
+         '.gitconfig', '.gitignore_global', '.clang-format', 'bin/diff-highlight',
+         '.tmux.conf', '.mbsyncrc', 'bin/read-password-emacs', '.gnupg/gpg-agent.conf',
+         'bin/ec', 'bin/ed']
 
 files.each do |file|
   cookbook_file "#{ENV['HOME']}/#{file}" do
