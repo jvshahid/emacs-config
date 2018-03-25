4b0fa7928fac0ba5bee013192f32f1005a1d0544
John Shahid <jvshahid@gmail.com>
2018-03-25 10:34:48 -0400

    use the default system pinentry
    pinentry-emacs interact in weird way with emacs and can make it hang sometimes

--- cookbooks/development/recipes/default.rb
+++ cookbooks/development/recipes/default.rb
@@ -14,7 +14,6 @@ return if node[:os] != 'linux'
 include_recipe "development::packages"
 include_recipe "development::cask"
 include_recipe "development::emacs"
-include_recipe "development::pinentry"
 include_recipe "development::tmux"
 include_recipe "development::setup_desktop_environment"
 include_recipe "development::z"
