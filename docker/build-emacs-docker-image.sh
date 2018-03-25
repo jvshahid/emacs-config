be8df4b539f9b207d7f044687d1e94fddf9aa65d
John Shahid <jvshahid@gmail.com>
2018-03-25 10:50:50 -0400

    auto push the docker image after a successful build

--- docker/build-emacs-docker-image.sh
+++ docker/build-emacs-docker-image.sh
@@ -7,3 +7,4 @@ trap 'docker rm -f build-emacs' EXIT
 docker pull ubuntu:latest
 docker run --security-opt seccomp=unconfined --name build-emacs -v $PWD:/scripts -it ubuntu:latest /scripts/build-emacs.sh
 docker commit build-emacs jvshahid/emacs:latest
+docker push jvshahid/emacs
