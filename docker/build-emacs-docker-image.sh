30e17ef625983620e04eeef4bef591e1d79651fb
John Shahid <jvshahid@gmail.com>
2018-03-30 09:27:06 -0400

    build the full development environment

--- docker/build-emacs-docker-image.sh
+++ docker/build-emacs-docker-image.sh
@@ -8,3 +8,5 @@ docker pull ubuntu:latest
 docker run --security-opt seccomp=unconfined --name build-emacs -v $PWD:/scripts -it ubuntu:latest /scripts/build-emacs.sh
 docker commit build-emacs jvshahid/emacs:latest
 docker push jvshahid/emacs
+docker build -t jvshahid/development .
+docker push jvshahid/development
