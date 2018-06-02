eeeb220f88034d6d6223306b8c83c44d97df5308
John Shahid <jvshahid@gmail.com>
2018-06-02 12:49:44 -0400

    fix the ubuntu version of the base image
    this way i always get consistent library versions

--- docker/build-emacs-docker-image.sh
+++ docker/build-emacs-docker-image.sh
@@ -4,8 +4,9 @@ set -e
 
 trap 'docker rm -f build-emacs' EXIT
 
-docker pull ubuntu:latest
-docker run --security-opt seccomp=unconfined --name build-emacs -v $PWD:/scripts -it ubuntu:latest /scripts/build-emacs.sh
+base_image=ubuntu:16.04
+docker pull $base_image
+docker run --security-opt seccomp=unconfined --name build-emacs -v $PWD:/scripts -it $base_image /scripts/build-emacs.sh
 docker commit build-emacs jvshahid/emacs:latest
 docker push jvshahid/emacs
 docker build -t jvshahid/development .
