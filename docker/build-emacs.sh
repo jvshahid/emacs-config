a728b95acb5379376c687d92ce3bee0a3c97bde2
John Shahid <jvshahid@gmail.com>
2018-06-15 16:56:10 -0400

    compile emacs with debug symbols

--- docker/build-emacs.sh
+++ docker/build-emacs.sh
@@ -16,11 +16,11 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone --depth 1 https://github.com/emacs-mirror/emacs
 pushd emacs
   autoreconf -i
-  ./configure --without-x
+  ./configure --enable-checking='yes,glyphs' --enable-check-lisp-object-type CFLAGS='-O0 -g3' --without-x
   make -j8
   make install
 popd
-rm -rf emacs
+# rm -rf emacs
 apt-get remove -y --purge build-essential autoconf gcc gcc-5
 apt-get autoremove -y
 rm -rf /var/lib/apt/lists
