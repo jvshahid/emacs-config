23d1b98239ae80e4deed52beb73c30a07a8b8ac5
John Shahid <jvshahid@gmail.com>
2018-08-01 17:56:41 -0400

    add .Xresources to setup the proper font and antialiasing

--- cookbooks/development/recipes/basic.rb
+++ cookbooks/development/recipes/basic.rb
@@ -5,7 +5,7 @@ files = ['alias.sh', 'functions.sh', 'exports.sh', 'prompt.sh',
          'bin/flavor', 'bin/cleanup_shit.sh', 'bin/setup_go_project.sh',
          '.gitconfig', '.gitignore_global', '.clang-format', 'bin/diff-highlight',
          '.tmux.conf', '.mbsyncrc', 'bin/read-password-emacs', '.gnupg/gpg-agent.conf',
-         'bin/ec', 'bin/ed', '.xinitrc', 'bin/suspend.sh']
+         'bin/ec', 'bin/ed', '.xinitrc', '.Xresources', 'bin/suspend.sh']
 
 files.each do |file|
   cookbook_file "#{ENV['HOME']}/#{file}" do
