6b15a5104a6f2d310a533d7bbd4cbe80e2a31154
John Shahid <jvshahid@gmail.com>
2018-10-13 13:56:01 -0400

    don't use newlines to break long lines in term
    text can reflow when the window is resized and can be copied properly, like tmux

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -403,6 +403,7 @@ the command again. CMD is the command to run"
  '(show-trailing-whitespace t)
  '(tab-width 2)
  '(tags-case-fold-search t)
+ '(term-suppress-hard-newline t)
  '(tool-bar-mode nil)
  '(user-full-name "John Shahid")
  '(user-mail-address "jvshahid@gmail.com")
