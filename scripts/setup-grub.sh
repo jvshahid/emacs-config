5af12110ba56f2f9b09365186783c882d8999899
John Shahid <jvshahid@gmail.com>
2018-12-02 17:56:28 -0500

    update-grub doesn't exist

--- scripts/setup-grub.sh
+++ scripts/setup-grub.sh
@@ -4,12 +4,10 @@ source ./helpers.sh
 
 install_package grub
 
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
 mkdir -p /boot/grub
 cd /boot/grub
 grub-mkconfig > grub.cfg
 
 device=$(df -h / | grep 'dev' | awk '{print $1}')
 grub-install ${device/[0-9]/}
-
-sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
-update-grub
