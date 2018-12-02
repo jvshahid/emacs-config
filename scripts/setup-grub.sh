d938de0cd7571778cff33da857c9ad6ed845bd29
John Shahid <jvshahid@gmail.com>
2018-12-02 12:15:32 -0500

    reduce the grub wait time to 0

--- scripts/setup-grub.sh
+++ scripts/setup-grub.sh
@@ -7,3 +7,6 @@ grub-mkconfig > grub.cfg
 
 device=$(df -h / | grep 'dev' | awk '{print $1}')
 grub-install ${device/[0-9]/}
+
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
+update-grub
