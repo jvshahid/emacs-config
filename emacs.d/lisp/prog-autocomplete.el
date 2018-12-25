7c46eba8796d018482ff27db909d2a92f44d7e48
John Shahid <jvshahid@gmail.com>
2018-12-25 15:05:28 -0500

    set autocomplete delay to 0

--- emacs.d/lisp/prog-autocomplete.el
+++ emacs.d/lisp/prog-autocomplete.el
@@ -5,7 +5,7 @@
 (global-company-mode)
 
 (with-eval-after-load 'company
-  (setq company-idle-delay 0.2)
+  (setq company-idle-delay 0)
   (setq company-minimum-prefix-length 0)
   (define-key company-active-map (kbd "M-d") 'company-show-doc-buffer)
   (define-key company-active-map (kbd "M-w") 'company-show-location)
