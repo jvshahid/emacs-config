ec153a5ff3d3f3be51e8549824fb07bf29e42603
John Shahid <jvshahid@gmail.com>
2018-12-25 15:00:29 -0500

    add flymake keybinding

--- emacs.d/lisp/prog-lsp.el
+++ emacs.d/lisp/prog-lsp.el
@@ -2,3 +2,8 @@
 
 (straight-use-package 'eglot)
 (straight-use-package 'company)
+
+(with-eval-after-load 'flymake
+  (define-key flymake-mode-map (kbd "C-c ! n") 'flymake-goto-next-error)
+  (define-key flymake-mode-map (kbd "C-c ! p") 'flymake-goto-prev-error)
+  (define-key flymake-mode-map (kbd "C-c ! l") 'flymake-show-diagnostics-buffer))
