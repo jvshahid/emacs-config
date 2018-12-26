0b63d8f5044eff61fa61c3f4a094b128e02294e5
John Shahid <jvshahid@gmail.com>
2018-12-26 14:59:18 -0500

    install emacs with jpg, giff and xpm support

--- scripts/install-emacs.sh
+++ scripts/install-emacs.sh
@@ -2,16 +2,12 @@
 
 source ./helpers.sh
 
-install_package gcc autoconf make gtk3 pkg-config git
+install_package gcc autoconf make gtk3 pkg-config git libtiff libxpm giflib libjpeg-turbo
 
 [ ! -d ~/codez/emacs ] && git clone https://github.com/emacs-mirror/emacs.git ~/codez/emacs
 
 pushd ~/codez/emacs
   [ ! -f ./configure ] && autoreconf -i
-  [ ! -f Makefile ] && ./configure --prefix=$HOME/bin/emacs-27\
-                                   --with-xpm=no\
-                                   --with-jpeg=no\
-                                   --with-gif=no\
-                                   --with-tiff=no
+  [ ! -f Makefile ] && ./configure --prefix=$HOME/bin/emacs-27
   make install
 popd
