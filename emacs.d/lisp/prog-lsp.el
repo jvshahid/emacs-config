df0fd3fcb7562aa7e8402c711d0d41b37649b9d3
John Shahid <jvshahid@gmail.com>
2018-12-27 13:12:04 -0500

    remove redundant configuration

--- emacs.d/lisp/prog-lsp.el
+++ emacs.d/lisp/prog-lsp.el
@@ -1,7 +1,6 @@
 ;;; -*- lexical-binding: t; -*-
 
 (straight-use-package 'eglot)
-(straight-use-package 'company)
 
 (with-eval-after-load 'flymake
   (define-key flymake-mode-map (kbd "C-c ! n") 'flymake-goto-next-error)
