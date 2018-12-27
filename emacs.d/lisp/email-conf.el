84c5a17dbd877787be3382df10c3e00a271b1af2
John Shahid <jvshahid@gmail.com>
2018-12-27 17:53:05 -0500

    make mu4e quieter

--- emacs.d/lisp/email-conf.el
+++ emacs.d/lisp/email-conf.el
@@ -3,6 +3,9 @@
 (add-to-list 'load-path "~/bin/mu/share/emacs/site-lisp/mu4e")
 (autoload 'mu4e "mu4e" "start mu4e" t)
 
+;; prevent mu4e update messages
+(setq mu4e-hide-index-messages t)
+
 ;; keep email details separately in an encrypted file.  Ignore errors if the
 ;; file is encrypted
 (ignore-errors
