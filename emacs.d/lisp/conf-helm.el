7714a0834d791c06388a8c71de1f5d85c1048205
John Shahid <jvshahid@gmail.com>
2019-02-12 09:06:47 -0500

    Turn on eshell fuzzy matching.

--- emacs.d/lisp/conf-helm.el
+++ emacs.d/lisp/conf-helm.el
@@ -2,12 +2,6 @@
 
 (straight-use-package 'helm)
 (straight-use-package 'helm-projectile)
-(require 'helm-projectile)
-(require 'helm-config)
-
-(global-set-key (kbd "C-c =") #'helm-show-kill-ring)
-(global-set-key (kbd "M-x") #'helm-M-x)
-(setq helm-exit-idle-delay 0)
 
 ;; helm fuzzy matching
 (setq helm-locate-fuzzy-match t)
@@ -15,7 +9,15 @@
 (setq helm-buffers-fuzzy-matching t)
 (setq helm-M-x-fuzzy-match t)
 (setq helm-mode-fuzzy-match t)
+(setq helm-eshell-fuzzy-match t)
 (setq helm-completion-in-region-fuzzy-match t)
+
+(require 'helm-projectile)
+(require 'helm-config)
+
+(global-set-key (kbd "C-c =") #'helm-show-kill-ring)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(setq helm-exit-idle-delay 0)
 
 ;; stop using a new frame for helm completion
 (setq helm-show-completion-display-function nil)
