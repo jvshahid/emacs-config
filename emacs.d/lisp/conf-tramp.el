a87a9dbf5a38661fac74c0fa6c7f6054c0d63939
John Shahid <jvshahid@gmail.com>
2019-03-29 09:16:44 -0400

    add normal remote PATH when using tramp

--- emacs.d/lisp/conf-tramp.el
+++ emacs.d/lisp/conf-tramp.el
@@ -2,4 +2,5 @@
 
 (with-eval-after-load 'tramp
   (push "~/.fzf/bin" tramp-remote-path)
-  (push "~/bin" tramp-remote-path))
+  (push "~/bin" tramp-remote-path)
+  (push 'tramp-own-remote-path tramp-remote-path))
