3fb2ca9351b630cf7d1643bf7545c039ef4c57c8
John Shahid <jvshahid@gmail.com>
2019-04-25 15:37:43 -0400

    Different strategy to fight helm trying to deactivate Ido

--- emacs.d/lisp/conf-ido.el
+++ emacs.d/lisp/conf-ido.el
@@ -3,20 +3,14 @@
 (straight-use-package 'flx-ido)
 
 ;; The wiki states that this shouldn't be done, but ido ends up not doing some
-;; important initialization.  This causes Ido not to remember the most
-;; frequently visited paths which is really a cool feature.
+;; important things, such as remembering the most frequently visited paths and
+;; listing them first in the completion list.  To get around this leave
+;; ido-mode enabled and disable the ability to disable it using an advice.
+
 (ido-mode t)
+(flx-ido-mode 1)
+(setq ido-use-faces nil)
 
-;; let helm know that I want to use ido instead
-(with-eval-after-load 'helm-mode
-  (add-to-list 'helm-completing-read-handlers-alist '(find-file . ido))
-  (add-to-list 'helm-completing-read-handlers-alist '(switch-to-buffer . ido))
-  (add-to-list 'helm-completing-read-handlers-alist '(kill-buffer . ido))
-  (add-to-list 'helm-completing-read-handlers-alist '(dired-do-rename . ido))
-  (add-to-list 'helm-completing-read-handlers-alist '(dired-do-copy . ido))
-  (add-to-list 'helm-completing-read-handlers-alist '(dired-create-directory . ido))
-  (add-to-list 'helm-completing-read-handlers-alist '(find-grep-current-word . ido)))
-
-(with-eval-after-load 'ido
-  (flx-ido-mode 1)
-  (setq ido-use-faces nil))
+;; do nothing
+(defun shahid/do-not-disable-ido (orig-func &rest args))
+(advice-add 'ido-mode :override #'shahid/do-not-disable-ido)
