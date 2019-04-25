7c43b197f04770ef2d5e75a7ace08a10d334400f
John Shahid <jvshahid@gmail.com>
2019-04-25 15:18:17 -0400

    Move org settings in with-eval-after-load

--- emacs.d/lisp/conf-org.el
+++ emacs.d/lisp/conf-org.el
@@ -6,10 +6,11 @@
 (add-hook 'org-mode-hook 'setup-org-keybindings)
 (add-hook 'org-mode-hook 'org-indent-mode)
 (add-hook 'org-mode-hook 'visual-line-mode)
-(add-to-list 'org-agenda-files "~/Dropbox/Orgzly")
-(shahid/bind-global-key "C-c C-a" #'org-agenda)
 
-(setq org-enforce-todo-dependencies t)
-(setq org-refile-use-outline-path 'file)
-(setq org-outline-path-complete-in-steps nil)
+(global-set-key (kbd "C-c C-a") #'org-agenda)
 
+(with-eval-after-load 'org
+  (add-to-list 'org-agenda-files "~/Dropbox/orgs")
+  (setq org-enforce-todo-dependencies t)
+  (setq org-refile-use-outline-path 'file)
+  (setq org-outline-path-complete-in-steps nil))
