abfc34ceb2a7d0df65fe2faff2e44cd157d59488
John Shahid <jvshahid@gmail.com>
2019-04-27 08:04:23 -0400

    More Org setup
    - more TODO states
    - logging when entry state changes
    - add capture template

--- emacs.d/lisp/conf-org.el
+++ emacs.d/lisp/conf-org.el
@@ -10,7 +10,16 @@
 (global-set-key (kbd "C-c C-a") #'org-agenda)
 
 (with-eval-after-load 'org
+  (setq org-directory "~/Dropbox/orgs")
   (add-to-list 'org-agenda-files "~/Dropbox/orgs")
   (setq org-enforce-todo-dependencies t)
   (setq org-refile-use-outline-path 'file)
-  (setq org-outline-path-complete-in-steps nil))
+  (setq org-outline-path-complete-in-steps nil)
+  (setq org-todo-keywords
+    '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELED(c@)")))
+  (setq org-log-into-drawer t)
+  (setq org-capture-templates
+   '(("t" "Todo" entry (file+headline "~/Dropbox/orgs/todo.org" "Inbox")
+          "* TODO %? %^g\n  %a"))))
+
+(define-key global-map "\C-cc" #'org-capture)
