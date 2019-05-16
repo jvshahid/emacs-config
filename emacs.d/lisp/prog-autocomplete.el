8e48c14d7ecbcd9daf5dc0938d3b9371aa2922c6
John Shahid <jvshahid@gmail.com>
2019-05-16 14:12:49 -0400

    Remove dabbrev backend from company

--- emacs.d/lisp/prog-autocomplete.el
+++ emacs.d/lisp/prog-autocomplete.el
@@ -13,3 +13,7 @@
   (define-key company-active-map (kbd "M-w") 'company-show-location)
   (delete 'company-eclim company-backends)
   (define-key company-mode-map (kbd "C-M-i") 'company-complete))
+
+;; this backend adds so much garbage and is not very usefull.  I usually use
+;; M-/ to expand dabbrev
+(setq company-backends (delete 'company-dabbrev company-backends))
