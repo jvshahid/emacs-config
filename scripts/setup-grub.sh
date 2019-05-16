975fe81ac51e5563e5cd5c6089a270f3e422fff1
John Shahid <jvshahid@gmail.com>
2019-05-16 14:13:57 -0400

    Update intell microcode

--- scripts/setup-grub.sh
+++ scripts/setup-grub.sh
@@ -2,12 +2,13 @@
 
 source ./helpers.sh
 
+install_package intell-ucode    # get intel microcode
 install_package grub
 
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
 mkdir -p /boot/grub
 cd /boot/grub
-grub-mkconfig > grub.cfg
+grub-mkconfig -o grub.cfg
 
 device=$(df -h / | grep 'dev' | awk '{print $1}')
 grub-install ${device/[0-9]/}
