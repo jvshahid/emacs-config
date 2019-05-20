dc6caab31e48513d0dbe258f5eb23347839bff1e
John Shahid <jvshahid@gmail.com>
2019-05-20 09:31:38 -0400

    Fix a typo

--- emacs.d/lisp/prog-terraform.el
+++ emacs.d/lisp/prog-terraform.el
@@ -2,6 +2,6 @@
 
 (straight-use-package 'terraform-mode)
 (straight-use-package 'company-terraform)
-(with-eval-after-load 'terraform
+(with-eval-after-load 'terraform-mode
   (company-terraform-init)
   (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode))
