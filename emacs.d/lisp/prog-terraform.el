594638503cf1c6edb1de7666f731f5244c8eae26
John Shahid <jvshahid@gmail.com>
2019-06-19 08:14:31 -0400

    Add hs-minor-mode to terraform hook

--- emacs.d/lisp/prog-terraform.el
+++ emacs.d/lisp/prog-terraform.el
@@ -4,4 +4,6 @@
 (straight-use-package 'company-terraform)
 (with-eval-after-load 'terraform-mode
   (company-terraform-init)
-  (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode))
+  (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
+  (add-hook 'terraform-mode-hook #'hs-minor-mode))
+
