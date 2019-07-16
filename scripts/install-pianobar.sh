1608fbac1dc657eec4867b199662b6ee6d7200d1
John Shahid <jvshahid@gmail.com>
2019-07-16 10:42:17 -0400

    Use specific commit of pianobar without audio buffering
    Audio buffering causes volume changes to be delayed.  See
    https://github.com/PromyLOPh/pianobar/issues/679 for more information.

--- scripts/install-pianobar.sh
+++ scripts/install-pianobar.sh
@@ -7,7 +7,9 @@ install_package ffmpeg libao
 [ ! -d ~/codez/pianobar ] && git clone https://github.com/PromyLOPh/pianobar.git ~/codez/pianobar
 
 pushd ~/codez/pianobar
-  git pull --rebase
+  # pianobar introduced buffering in the following commit which causes volume
+  # changes to be delayed.  Use the previous commit to avoid that issue.
+  git checkout bbbdd99fdaacfe0a3c9a1237fa0d086907c1286f~
   make
 popd
 
