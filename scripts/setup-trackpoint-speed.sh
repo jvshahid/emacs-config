08b88c9ca95805f9a08a3f5c9c402053402db3bb
John Shahid <jvshahid@gmail.com>
2019-07-17 08:02:02 -0400

    Override the change added to libinput in order to slow down the acceleration

--- scripts/setup-trackpoint-speed.sh
+++ scripts/setup-trackpoint-speed.sh
@@ -13,3 +13,13 @@ SUBSYSTEM=="serio", ATTR{protocol}=="TPPS/2", ATTR{sensitivity}="250"
 EOF
 
 $sudo udevadm control --reload
+
+# override the acceleration multiplier introduced in this commit:
+# https://github.com/wayland-project/libinput/commit/305387be
+$sudo mkdir -p /etc/libinput/
+cat <<EOF | $sudo tee /etc/libinput/local-overrides.quirks
+[Lenovo X230 Trackpoint]
+MatchName=*TPPS/2 IBM TrackPoint
+MatchDMIModalias=dmi:*svnLENOVO:*pvrThinkPadX230:*
+AttrTrackpointMultiplier=1.0
+EOF
