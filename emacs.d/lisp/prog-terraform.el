25510cd0e5d28c4de57910cd29c94dda602d45ca
John Shahid <jvshahid@gmail.com>
2019-08-05 12:31:45 -0400

    Turn on subword-mode in terraform buffers

--- emacs.d/lisp/prog-terraform.el
+++ emacs.d/lisp/prog-terraform.el
@@ -5,5 +5,5 @@
 (with-eval-after-load 'terraform-mode
   (company-terraform-init)
   (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
-  (add-hook 'terraform-mode-hook #'hs-minor-mode))
-
+  (add-hook 'terraform-mode-hook #'hs-minor-mode)
+  (add-hook 'terraform-mode-hook #'subword-mode))
