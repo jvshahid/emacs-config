259197b39302c4af75352f23e95f37641162c4b5
John Shahid <jvshahid@gmail.com>
2019-09-03 10:31:40 -0400

    Fix eshell history keybinding

--- emacs.d/lisp/conf-term.el
+++ emacs.d/lisp/conf-term.el
@@ -65,7 +65,7 @@ With prefix argument, switch to default-directory"
 (add-hook 'eshell-mode-hook
           (lambda ()
             (eshell-cmpl-initialize)
-            (define-key eshell-mode-map (kbd "M-r") 'helm-eshell-history)))
+            (define-key eshell-hist-mode-map (kbd "M-r") 'helm-eshell-history)))
 
 (defun eshell-confirm-kill (func &rest args)
   (when (y-or-n-p (format "Kill eshell process "))
