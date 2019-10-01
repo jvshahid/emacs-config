4548a4a0fbedcdaf946dcfa757ea63c2b8fd9aa7
John Shahid <jvshahid@gmail.com>
2019-10-01 09:50:28 -0400

    Use inf-clojure instead of cider
    It is way simpler than cider but has less features.

--- emacs.d/lisp/prog-clojure.el
+++ emacs.d/lisp/prog-clojure.el
@@ -2,7 +2,7 @@
 
 (straight-use-package 'clojure-mode)
 (straight-use-package 'flycheck-clojure)
-(straight-use-package 'cider)
+(straight-use-package 'inf-clojure)
 
 (with-eval-after-load 'clojure-mode
   (add-hook 'clojure-mode-hook 'parinfer-mode)
@@ -12,10 +12,6 @@
   (add-hook 'clojure-mode-hook (lambda ()
                                  (local-set-key (kbd "C-c C-r") 'projectile-toggle-between-implementation-and-test))))
 
-(with-eval-after-load 'cider
-  (flycheck-clojure-setup))
-
-(with-eval-after-load 'cider-mode
-  (define-key cider-mode-map (kbd "C-M-i") nil))
+(add-hook 'clojure-mode-hook #'inf-clojure-minor-mode)
 
 
