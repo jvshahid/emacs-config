2ed73a4d44a17f4631e3d3b57ef671cee96d61db
John Shahid <jvshahid@gmail.com>
2019-10-15 13:18:23 -0400

    Use brave instead of firefox

--- scripts/install-browser.sh
+++ scripts/install-browser.sh
@@ -4,9 +4,7 @@ set -x
 
 source ./helpers.sh
 
-installe_package firefox
+install_package brave-bin
 install_package libu2f-host     # yubi key access from browser
-xdg-mime default firefox.desktop x-scheme-handler/https
-xdg-mime default firefox.desktop x-scheme-handler/http
 
 
