49ec2dd4c45040cbe2d792fa7aa47067e04a7479
John Shahid <jvshahid@gmail.com>
2019-12-08 09:49:16 -0500

    Switch back to cider

--- emacs.d/lisp/prog-clojure.el
+++ emacs.d/lisp/prog-clojure.el
@@ -3,7 +3,6 @@
 (straight-use-package 'cider)
 (straight-use-package 'clojure-mode)
 (straight-use-package 'flycheck-clojure)
-(straight-use-package 'inf-clojure)
 
 (with-eval-after-load 'clojure-mode
   (add-hook 'clojure-mode-hook 'parinfer-mode)
@@ -13,6 +12,10 @@
   (add-hook 'clojure-mode-hook (lambda ()
                                  (local-set-key (kbd "C-c C-r") 'projectile-toggle-between-implementation-and-test))))
 
-(add-hook 'clojure-mode-hook #'inf-clojure-minor-mode)
+(with-eval-after-load 'cider
+  (flycheck-clojure-setup))
+
+(with-eval-after-load 'cider-mode
+  (define-key cider-mode-map (kbd "C-M-i") nil))
 
 
