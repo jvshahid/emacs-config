dcd8bb5dcb59e44499731e21b41bbbb504b02e1c
John Shahid <jshahid@pivotal.io>
2020-01-07 17:14:25 -0500

    Get rid of helm in favor of ivy

--- emacs.d/lisp/conf-helm.el
+++ emacs.d/lisp/conf-helm.el
@@ -1,41 +1,13 @@
 ;;; -*- lexical-binding: t; -*-
 
-(add-to-list 'display-buffer-alist
-             '("\\*helm.*" display-buffer-in-side-window
-               (side . bottom)
-               (window-height . 20)))
-
 (ido-mode -1)                           ; turn off ido-mode
 
-(straight-use-package 'helm)
-(straight-use-package 'helm-projectile)
+(straight-use-package 'ivy)
+(straight-use-package 'counsel)
 
-;; helm fuzzy matching
-(setq helm-completion-style 'emacs)
-(add-to-list 'completion-styles 'flex)
-(setq helm-locate-fuzzy-match t)
-(setq helm-recentf-fuzzy-match t)
-(setq helm-buffers-fuzzy-matching t)
+(ivy-mode)
 
-(setq helm-ff-skip-boring-files t)
-(setq helm-grep-file-path-style 'relative)
-(setq helm-substitute-in-filename-stay-on-remote t)
-(setq helm-prevent-escaping-from-minibuffer nil)
-
-(require 'helm-projectile)
-(require 'helm-config)
-
-(global-set-key (kbd "C-c =") #'helm-show-kill-ring)
-(global-set-key (kbd "M-x") #'helm-M-x)
-(global-set-key (kbd "C-x C-f") #'helm-find-files)
-
-;; stop using a new frame for helm completion
-(setq helm-show-completion-display-function nil)
-
-;;; override the default fzf find command
-(global-set-key (kbd "C-x C-p") 'helm-projectile-find-file)
-(global-set-key (kbd "C-x b") 'helm-buffers-list)
-
-(helm-mode 1)
-
-(global-set-key (kbd "M-s o") 'helm-occur)
+(setq projectile-completion-system 'ivy)
+(setq ivy-use-virtual-buffers t)
+(global-set-key (kbd "C-x C-p") 'projectile-find-file)
+(global-set-key (kbd "M-s o") 'swiper-isearch)
