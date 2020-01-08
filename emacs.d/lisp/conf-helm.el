1e3daa9377d88176a15552406115de8c261cf479
John Shahid <jshahid@pivotal.io>
2020-01-08 15:30:05 +0000

    Fix a few things with ivy
    Authored-by: John Shahid <jshahid@pivotal.io>

--- emacs.d/lisp/conf-helm.el
+++ emacs.d/lisp/conf-helm.el
@@ -3,10 +3,16 @@
 (ido-mode -1)                           ; turn off ido-mode
 
 (straight-use-package 'ivy)
+(straight-use-package 'ivy-hydra)
 (straight-use-package 'counsel)
+
+(setq ivy-virtual-abbreviate 'abbreviate
+      ivy-use-virtual-buffers t)
 
 (ivy-mode)
 
+(define-key ivy-minibuffer-map (kbd "C-m") 'ivy-alt-done)
+(define-key ivy-minibuffer-map (kbd "C-j") 'ivy-done)
+
 (setq projectile-completion-system 'ivy)
-(setq ivy-use-virtual-buffers t)
 (global-set-key (kbd "C-x C-p") 'projectile-find-file)
