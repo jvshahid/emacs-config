d0ac1e78baaab8ea86c23be564e03622ae3e0aa2
John Shahid <jvshahid@gmail.com>
2020-11-10 10:15:09 -0500

    Remove the mosh alias

--- dotfiles/alias.sh
+++ dotfiles/alias.sh
@@ -9,7 +9,6 @@ alias ack='ack-grep --no-group'
 alias run_query=~/Documents/benchmark/one-tick-scripts/run_query.sh
 alias xclipc='xclip -selection clipboard'
 alias gen_tags="$HOME/Documents/generate_tags.sh"
-alias mosh="$MOSH_INSTALLATION/bin/mosh --client=$MOSH_INSTALLATION/bin/mosh-client --server='$MOSH_INSTALLATION/bin/mosh-server'"
 alias wdiff="wdiff -n -w $'\033[1;31m' -x $'\033[0m' -y $'\033[1;34m' -z $'\033[0m'"
 alias ll='ls -l -a --color'
 # dsh interactive, wait for new commands and execute them
