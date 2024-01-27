ae91c882c7dd8d634b2a5c2648c7822bb906e1b5
John Shahid <jvshahid@gmail.com>
2024-01-27 18:57:25 -0500

    fix emacs-eat

--- emacs.d/lisp/conf-term.el
+++ emacs.d/lisp/conf-term.el
@@ -53,7 +53,15 @@
 
 (advice-add 'eshell-kill-process :around #'eshell-confirm-kill)
 
-(straight-use-package 'eat)
+(straight-use-package
+ '(eat :type git
+       :host codeberg
+       :repo "akib/emacs-eat"
+       :files ("*.el" ("term" "term/*.el") "*.texi"
+               "*.ti" ("terminfo/e" "terminfo/e/*")
+               ("terminfo/65" "terminfo/65/*")
+               ("integration" "integration/*")
+               (:exclude ".dir-locals.el" "*-tests.el"))))
 (add-hook 'eshell-load-hook #'eat-eshell-mode)
-
-
+;; eat is good enough in eshell
+(setq eshell-visual-commands nil)
