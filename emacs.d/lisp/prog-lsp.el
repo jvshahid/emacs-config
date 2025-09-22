7b00bae31cd9b4fe6dfd626536e8a9df1149b543
John Shahid <jvshahid@gmail.com>
2025-09-22 18:36:45 -0400

    Fix an issue with eglot

--- emacs.d/lisp/prog-lsp.el
+++ emacs.d/lisp/prog-lsp.el
@@ -5,4 +5,7 @@
   (define-key flymake-mode-map (kbd "C-c ! p") 'flymake-goto-prev-error)
   (define-key flymake-mode-map (kbd "C-c ! l") 'flymake-show-diagnostics-buffer))
 
+(require-with-check 'project nil 'reload)
+(require-with-check 'xref nil 'reload)
 (straight-use-package 'eglot)
+
